5826625e568964febb2eb18c2fe7b957fc29aeb8
Ayush Singh <theayushsingh72@gmail.com>
2022-04-04 19:22:13 +0530

    serial

--- test codes/modules/testSerial.py
+++ test codes/modules/testSerial.py
@@ -1,9 +1,14 @@
+from torch import R
 import serial
-if __name__ == '__main__':
+
+def readSerial():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
+    if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+
+
+if __name__ == '__main__':
+    readSerial()
 
