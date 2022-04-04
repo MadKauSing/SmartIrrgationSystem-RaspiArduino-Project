1728b11400f71ea0944c8c2ec6207b98757e5076
Ayush Singh <theayushsingh72@gmail.com>
2022-04-04 19:12:59 +0530

    check

--- test codes/soil_sensor/soil_sensor.ino
+++ test codes/soil_sensor/soil_sensor.ino
@@ -1,29 +1,51 @@
 //Sensor pins
-#define sensorPower 7
-#define sensorPin A0
+#define sensorPowerSoil 7
+#define sensorPowerLight 3
+#define sensorPinSoil A0
+#define sensorPinLight A3
+int sensorValueLight=0;
 
 void setup() {
-  pinMode(sensorPower, OUTPUT);
   
-  // Initially keep the sensor OFF
-  digitalWrite(sensorPower, LOW);
+  pinMode(sensorPowerSoil, OUTPUT);
+  digitalWrite(sensorPowerSoil, LOW);
+
+  pinMode(sensorPowerLight, OUTPUT);
+  digitalWrite(sensorPowerLight, LOW);
   
   Serial.begin(9600);
 }
 
 void loop() {
   //get the reading from the function below and print it
-  Serial.print("Analog output: ");
+
+  //sensorValueLight=analogRead(sensorPinLight);
+  Serial.print("S: ");
   Serial.println(readSensor());
+  Serial.print("L: ");
+  Serial.println(readSensorLight());
   
-  delay(1000);
+  //Serial.print("L:"); 
+  //Serial.println(sensorValueLight);
+
+  
+  delay(2000);
 }
+
+int readSensorLight(){
+  digitalWrite(sensorPowerLight, HIGH);  // Turn the sensor ON
+  delay(10);              // Allow power to settle
+  int val = analogRead(sensorPinLight);  // Read the analog value form sensor
+  digitalWrite(sensorPowerLight, LOW);   // Turn the sensor OFF
+  return val;     
+}
+
 
 //  This function returns the analog soil moisture measurement
 int readSensor() {
-  digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
+  digitalWrite(sensorPowerSoil, HIGH);  // Turn the sensor ON
   delay(10);              // Allow power to settle
-  int val = analogRead(sensorPin);  // Read the analog value form sensor
-  digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+  int val = analogRead(sensorPinSoil);  // Read the analog value form sensor
+  digitalWrite(sensorPowerSoil, LOW);   // Turn the sensor OFF
   return val;             // Return analog moisture value
 }
