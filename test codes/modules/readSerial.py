58ecc21dd01ce190f1a7f1160c7168d8ef0dd91e
Ayush Singh <theayushsingh72@gmail.com>
2022-04-04 19:34:20 +0530

    serial

--- test codes/modules/readSerial.py
+++ test codes/modules/readSerial.py
@@ -1,13 +1,14 @@
+from ast import While
 import serial
 
 
 def readSerial():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
-
-    if ser.in_waiting > 0:
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
 
 
 
