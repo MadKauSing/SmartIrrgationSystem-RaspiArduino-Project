558fe61d59355816bffcd3a9449fd2b08503feea
Ayush Singh <theayushsingh72@gmail.com>
2022-04-04 22:16:34 +0530

    some codes

---
+++ modules/dataDump.py
@@ -0,0 +1,14 @@
+import csv
+import datetime
+
+#fields = ["Date/Time", "temperature", "Moisture", "Humidity", "Light", "Height"]
+
+def writeValues(temperature,moisture,humidity,light,height):
+    file="../PlantData.csv"
+
+    with open(file, "a") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        current_time = datetime.datetime.now()
+        
+        row=[current_time,temperature,moisture,humidity,light,height]
+        csvwriter.writerow(row)
