51f3f5fec626f94677db7bd7842070f9f280dbd9
Ayush Singh <theayushsingh72@gmail.com>
2022-04-04 22:16:29 +0530

    somecodes

--- main.py
+++ main.py
@@ -4,5 +4,14 @@ sys.path.append('./modules/')
 import readSerial;
 import moist;
 import ultrasonic_distance;
+import dataDump;
 
+import time
 
+#main loop that runs in the background
+while True:
+    light,soil=readSerial.readSerial();
+
+    moistureSensor=moist.MoistureSensor(17);
+    ultraSensor=ultrasonic_distance.UltrasonicSensor(18,14)
+    
